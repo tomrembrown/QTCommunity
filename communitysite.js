6910eb80e11310c8a8fdd349bcd30e91d54eecd3
Tom Brown <tombrown654321@gmail.com>
2017-10-27 21:30:27 -0400

    Created vendor directory and added test environments.
    Moved bootstrap css to vendor directory, added mocha and chai, and the test for a test route.   Did not work.  Got server error 500.

--- communitysite.js
+++ communitysite.js
@@ -11,8 +11,18 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// Run static files out of pubic directory
 app.use(express.static(__dirname + '/public'));
 
+// To check if test environment
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+  console.log('Tests: ' + req.locals.showTests);
+  next();
+});
+
+// Routes
 app.get('/', function(req, res){
   var randomQuote = quotes.getQuote();
   res.render('home', {quote: randomQuote.quote, person: randomQuote.person});
