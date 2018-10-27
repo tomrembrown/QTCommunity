37a27035a3f8b729b8d943a91cdc35db3b962481
Thomas Brown <tom@tomrembrown.com>
2018-10-27 11:05:10 -0400

    Added license file
    Added a standard proprietary, closed-source license file.

--- communitysite.js
+++ communitysite.js
@@ -18,7 +18,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' &&
     req.query.test === '1';
-  console.log('Tests: ' + req.locals.showTests);
   next();
 });
 
