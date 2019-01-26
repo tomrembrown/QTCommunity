102bbe1b1611feca249ece8c785e783de39e4704
David <davidyim@Davids-Mac-mini.local>
2019-01-26 16:54:21 -0500

    Added InsideOut Parser

--- lab/qtfetch.js
+++ lab/qtfetch.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const https = require('https');
 const microdata = require('microdata-node');
 
 module.exports = { //https://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
 	getURL:function(url){
+		var designate = url.startsWith('https') ? https : http;
+		
 		return new Promise((resolve, reject)=>{ //https://stackoverflow.com/questions/41470296/how-to-await-and-return-the-result-of-a-http-request-so-that-multiple-request
-			http.get(url, (response)=>{ //https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
+			designate.get(url, (response)=>{ //https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
 				let data = '';
 				
 				response.on('data', (chunk)=>{
