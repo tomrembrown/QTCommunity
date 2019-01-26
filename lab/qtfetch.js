fa7f1f56c3b438772605036462368d64c812a909
David <davidyim@Davids-Mac-mini.local>
2019-01-26 15:42:49 -0500

    Established lab common functions

---
+++ lab/qtfetch.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const microdata = require('microdata-node');
+
+module.exports = { //https://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
+	getURL:function(url){
+		return new Promise((resolve, reject)=>{ //https://stackoverflow.com/questions/41470296/how-to-await-and-return-the-result-of-a-http-request-so-that-multiple-request
+			http.get(url, (response)=>{ //https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
+				let data = '';
+				
+				response.on('data', (chunk)=>{
+					data += chunk;
+				});
+				
+				response.on('end', ()=>{
+					resolve({'url':url, 'data':data});
+				});
+			}).on("error", (error)=>{
+				reject(error);
+			});					
+		});		
+	},
+	parseMicrodata:function(data){
+		var json = microdata.toJson(data, { //parse microdata
+			base: 'http://www.example.com'
+		});
+		
+		return json;
+	}
+};
+
+
