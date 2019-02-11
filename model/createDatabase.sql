9351b02cc1b8c3e55cd0ca265f89ca70b0e16187
Tom Brown <tom@tomrembrown.com>
2019-02-11 09:23:48 -0500

    Added table in database for quotations
    Added a table in the database for the quotations.  Next plan to modify the index.js file to get the quotations from the database instead of a flat file - will then delete the lib directory.

--- model/createDatabase.sql
+++ model/createDatabase.sql
@@ -14,12 +14,20 @@ DROP TABLE IF EXISTS organizations;
 DROP TABLE IF EXISTS places;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS organization_types;
+DROP TABLE IF EXISTS quotations;
 
 -- Drop tables with the older names
 DROP TABLE IF EXISTS member_types;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS locales;
 DROP TABLE IF EXISTS events;
+
+-- This is a standalone table - for quotations
+CREATE TABLE quotations(
+  id SERIAL PRIMARY KEY,
+  quotation TEXT NOT NULL,
+  person TEXT NOT NULL
+);
 
 -- Now, create them - from simplest and depends on nothing - to more complex
 CREATE TABLE organization_types(
@@ -173,3 +181,50 @@ CREATE TABLE categories_event_groups_relations(
   event_group_id INTEGER REFERENCES event_groups(id),
   PRIMARY KEY(category_id, event_group_id)
 );
+
+-- ****
+-- Insert initial data
+-- ****
+
+INSERT INTO quotations
+  (quotation, person) VALUES
+  ('There will not be a magic day when we wake up and it''s now okay to express ourselves publicly.  We make that day by doing things publicly until it''s simply the way things are.',
+    'Senator Tammy Baldwin'),
+  ('It takes some intelligence and insight to figure out you''re gay and then a tremendous amount of balls to live it and live it proudly.',
+    'Jason Bateman'),
+  ('I believe that no one should ever have to choose between a career we love and living our lives with authenticity and integrity',
+    'Selisse Berry'),
+  ('I''ve been embraced by a new community. That''s what happens when you''re finally honest about who you are; you find others like you.',
+    'Chaz Bono'),
+  ('Openness may not completely disarm prejudice, but it''s a good place to start.',
+    'Jason Collins'),
+  ('I think being gay is a blessing, and it''s something I am thankful for every single day.',
+    'Anderson Cooper'),
+  ('It is revolutionary for any trans person to choose to be seen and visible in a world that tells us we should not exist.',
+    'Laverne Cox'),
+  ('The beauty of standing up for your rights is others see you standing and stand up as well.',
+    'Cassandra Duffy'),
+  ('Never be bullied into silence. Never allow yourself to be made a victim. Accept no one''s definition of your life; define yourself.',
+    'Harvey Fierstein'),
+  ('I hate the word homophobia. It''s not a phobia. You''re not scared. You''re an asshole.',
+    'Morgan Freeman'),
+  ('I very much want to inject gay culture into the mainstream.  It''s not an underground tool for me.  It''s my whole life.', 
+    'Lady Gaga'),
+  ('Why is it that, as a culture, we are more comfortable seeing two men holding guns than holding hands?',
+    'Ernest J. Gaines'),
+  ('Personally, coming out was one of the most important things I''ve ever done, lifting from my shoulders the millstone of lies that I hadn''t even realized I was carrying.',
+    'Sir Ian McKellen'),
+  ('When all Americans are treated as equal, no matter who they are or whom they love, we are all more free.',
+    'Barack Obama'),
+  ('This world would be a whole lot better if we just made an effort to be less horrible to one another.',
+    'Ellen Page'),
+  ('Every gay and lesbian person who has been lucky enough to survive thee turmoil of growing up is a survivor.  Survivors have an obligation to those who will face the same challenges.',
+    'Bob Paris'),
+  ('If Harry Potter taught us anything it''s that no one should live in a closet.',
+    'J.K. Rowling'),
+  ('The pressures to gay teens can be overwhelming - to keep secrets, tell lies, deny who you are, and try to be who you''re not.  Remember: you are special and worth being cared about, loved, and accepted just as you are.  Never, ever let anyone convince you otherwise.',
+    'Alex Sanchez'),
+  ('We should indeed keep calm in the face of difference, and live our lives in a state of inclusion and wonder at the diversity of humanity.',
+    'George Takei'),
+  ('Please remembeer, especially in these times of group-think and the right-on chorus, that no person is your friend (or kin) who demands your silence, or denies your right to grow and be perceived as fully blossomeed as you were intended.',
+    'Alice Walker');
