a4423e0ad69761a9aaee0c5612b08f064054530c
Tom Brown <tom@tomrembrown.com>
2019-02-16 16:32:37 -0500

    Refactored script.js
    Refactored scripts.js to have less redundant code using different Vue object properties.  Also, adjusted syntax to condensed vue syntax in index.html and fixed spelling error in quote in createDatabase.sql

--- vm-client/public/js/script.js
+++ vm-client/public/js/script.js
@@ -1,117 +1,77 @@
+'use strict';
+/*
+ * script
+ *
+ * This contains the client-side view-model for the main page, using Vue.js
+ * 
+ * Modification History
+ * 
+ * Date             Name                Description
+ * February, 2019   Thomas Brown        Initial Creation
+ * 
+ */
+
+// Close as can get to enumerated type in JS: https://stackoverflow.com/questions/287903/what-is-the-preferred-syntax-for-defining-enums-in-javascript
+const PageEnum = Object.freeze(
+  {"calendar":0, "contact":1,"about":2,"addEvent":3,
+  "login":4,"editDeleteEvents":5, "registerOrganization":6,"addVolunteer":7,
+  "editDeleteVolunteer":8,"organizations":9,"volunteer":10});
+
+// What to display as header in each page - order corresponds to order in PageEnum
+const HEADERS = [
+  'Events','Contact Us', 'About Queer Toronto','Add Event', 
+  'Login', 'Edit / Delete Events', 'Register Organization','Add Volunteer Opportunity',
+  'Edit / Delete Volunteer Opportunity', 'Organizations', 'Volunteer Opportunities'
+];
+
 new Vue({
 	el: '#app',
 	data: {
 		quotation: 'to put here',
-		person: 'a person',
-		header: '',
-		isCalendar: false,
-		isContact: false,
-		isAbout: false,
-		isAddEvent: false,
-		isLogin: false,
-		isEditDeleteEvents: false,
-		isRegisterOrganization: false,
-		isAddVolunteer: false,
-		isEditDeleteVolunteer: false,
-		isOrganizations: false,
-		isVolunteer: false
+    person: 'a person',
+    page: PageEnum.calendar
 	},
 	methods: {
-		displayCalendar: function() {
-			this.getRandomQuotation();
-			this.header = 'Events';
-			this.setAllLinksFalse();
-			this.isCalendar = true;
-		},
-		displayContact: function() {
-			this.getRandomQuotation();
-			this.header = 'Contact Us';
-			this.setAllLinksFalse();
-			this.isContact = true;
-		},
-		displayAbout: function() {
-			this.getRandomQuotation();
-			this.header = 'About Queer Toronto';
-			this.setAllLinksFalse();
-			this.isAbout = true;
-		},
-		displayAddEvent: function() {
-			this.getRandomQuotation();
-			this.header = 'Add Event';
-			this.setAllLinksFalse();
-			this.isAddEvent = true;
-		},
-		displayLogin: function() {
-			this.getRandomQuotation();
-			this.header = 'Login';
-			this.setAllLinksFalse();
-			this.isLogin = true;
-		},
-		displayEditDeleteEvents: function() {
-			this.getRandomQuotation();
-			this.header = 'Edit / Delete Events';
-			this.setAllLinksFalse();
-			this.isEditDeleteEvents = true;
-		},
-		displayEditOrganization: function() {
-			this.getRandomQuotation();
-			this.header = 'Edit Organization';
-			this.setAllLinksFalse();
-			this.isEditOrganization = true;
-		},
-		displayRegisterOrganization: function() {
-			this.getRandomQuotation();
-			this.header = 'Register Organization';
-			this.setAllLinksFalse();
-			this.isRegisterOrganization = true;
-		},
-		displayAddVolunteer: function() {
-			this.getRandomQuotation();
-			this.header = 'Add Volunteer Opportunity';
-			this.setAllLinksFalse();
-			this.isAddVolunteer = true;
-		},
-		displayEditDeleteVolunteer: function() {
-			this.getRandomQuotation();
-			this.header = 'Edit / Delete Volunteer Opportunity';
-			this.setAllLinksFalse();
-			this.isEditDeleteVolunteer = true;
-		},
-		displayOrganizations: function() {
-			this.getRandomQuotation();
-			this.header = 'Organizations';
-			this.setAllLinksFalse();
-			this.isOrganizations = true;
-		},
-		displayVolunteer: function() {
-			this.getRandomQuotation();
-			this.header = 'Volunteer Opportunities';
-			this.setAllLinksFalse();
-			this.isVolunteer = true;
-		},
-		getRandomQuotation: function() {
-			this.$http
-				.get('/ajaxRoutes/getRandomQuotation')
-				.then(function(res) {
-					this.quotation = res.data.quotation;
-					this.person = res.data.person;
-				});
-		},
-		setAllLinksFalse: function() {
-			this.isCalendar = false;
-			this.isContact = false;
-			this.isAbout = false;
-			this.isAddEvent = false;
-			this.isLogin = false;
-			this.isEditDeleteEvents = false;
-			this.isRegisterOrganizations = false;
-			this.isAddVolunteer = false;
-			this.isEditDeleteVolunteer = false;
-			this.isOrganizations = false;
-			this.isVolunteer = false;
+		displayCalendar: function() { this.page = PageEnum.calendar; },
+    displayContact: function() { this.page = PageEnum.contact; },
+		displayAbout: function() { this.page = PageEnum.about; },
+		displayAddEvent: function() { this.page = PageEnum.addEvent; },
+		displayLogin: function() { this.page = PageEnum.login; },
+		displayEditDeleteEvents: function() { this.page = PageEnum.editDeleteEvents; },
+    displayRegisterOrganization: function() { this.page = PageEnum.registerOrganization; },
+    displayAddVolunteer: function() { this.page = PageEnum.addVolunteer; },
+    displayEditDeleteVolunteer: function() { this.page = PageEnum.editDeleteVolunteer; },
+    displayOrganizations: function() { this.page = PageEnum.organizations; },
+    displayVolunteer: function() { this.page = PageEnum.volunteer; },
+    getRandomQuotation: function() {
+      this.$http
+        .get('/ajaxRoutes/getRandomQuotation')
+        .then(function(res) {
+              this.quotation = res.data.quotation;
+              this.person = res.data.person;
+        });
+    }
+  },
+  computed: {
+    header: function() { return HEADERS[this.page]; },
+		isCalendar: function() { return this.page === PageEnum.calendar; },
+		isContact: function() { return this.page === PageEnum.contact; },
+		isAbout: function() { return this.page === PageEnum.about; },
+		isAddEvent: function() { return this.page === PageEnum.addEvent; },
+		isLogin: function() { return this.page === PageEnum.login; },
+		isEditDeleteEvents: function() { return this.page === PageEnum.editDeleteEvents; },
+		isRegisterOrganization: function() { return this.page === PageEnum.registerOrganization; },
+		isAddVolunteer: function() { return this.page === PageEnum.addVolunteer; },
+		isEditDeleteVolunteer: function() { return this.page === PageEnum.editDeleteVolunteer; },
+		isOrganizations: function() { return this.page === PageEnum.organizations; },
+		isVolunteer: function() { return this.page === PageEnum.volunteer; }
+  },
+  watch: {
+    page: function(newPage, oldPage) {
+      this.getRandomQuotation();
 		}
-	},
-	beforeMount() {
-		this.displayCalendar();
-	}
+  },
+  beforeMount() {
+    this.getRandomQuotation()
+  }
 });
