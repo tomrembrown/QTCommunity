2ec790a22ff379be323436a7cc735ff48c6fe5c1
David <davidyim@Davids-Mac-mini.local>
2019-07-01 21:20:53 -0400

    Events: started adding events

--- client/view/components/formElements/timeAndDate.vue
+++ client/view/components/formElements/timeAndDate.vue
@@ -5,7 +5,7 @@
         <h2>Time and Date</h2>
       </div>
     </div>
-    <div class="row align-items-center">
+<!--    <div class="row align-items-center">
       <div class="col-md-6">
         <section>
           <label class="label" for="startTime">Start Date and Time</label>
@@ -41,6 +41,33 @@
           </label>
         </section>
       </div>
+    </div>-->
+    
+    <div class="row align-items-center" v-for="(instance, index) in instances">
+      <div class="col-md-6">
+        <section>
+          <label class="label" for="startTime">Start Date and Time</label>
+          <my-date-time-box :myInputId="'startTime' + index"></my-date-time-box>
+          <div class="note" id="startTimeHelp">
+            Enter the start date and time of the event
+          </div>
+        </section>
+      </div>
+      <div class="col-md-6">
+        <section>
+          <label class="label" for="endTime">End Date and Time</label>
+          <my-date-time-box :myInputId="'endTime-' + index"></my-date-time-box>
+          <div class="note" id="endTimeHelp">
+            Enter the end date and time of the event
+          </div>
+        </section>
+      </div>      
+    </div>
+    
+    <div class="row">
+      <div class="col-md-12">
+      <button v-on:click.prevent="addTime">Add More Time(s)</button>
+      </div>
     </div>
   </fieldset>
 </template>
@@ -51,7 +78,17 @@ import MyDateTimeBox from './myDateTimeBox.vue'
 export default {
   components: {
     'my-date-time-box': MyDateTimeBox
-  }
+  },
+  data() {
+    return {
+	  instances:[]
+    }
+  },
+  methods:{
+	  addTime(){
+		  this.instances.push({});
+	  }
+  } 
 }
 </script>
 
