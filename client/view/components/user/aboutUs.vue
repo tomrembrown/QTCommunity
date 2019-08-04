af8ecf283602ba1eb58e33a1fddc3e478b654457
Tom Brown <tom@tomrembrown.com>
2019-08-04 00:19:31 -0400

    Finished About Us and updated To-do list
    Finished About US page and updated the to-do list.

--- client/view/components/user/aboutUs.vue
+++ client/view/components/user/aboutUs.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="main-box">
       <h2>About Us</h2>
-      <p>This website, QueerToronto, is the first in series of community websites built by the social 
-        economy enterprise, Ashroh Enterprises. The vision, the why, of this company, Ashroh Enterprises, 
-        is to fight the loneliness epidemic by helping people build more authentic relationships, and by 
+      <p>This website, Queer Toronto, is the first in series of community websites built by the social 
+        economy enterprise, Ashroh Enterprises. The vision, the why, of Ashroh Enterprises 
+        is to fight the loneliness by helping people build more authentic relationships and by 
         building community.</p>
       <p>There are many ways to build community and fight loneliness. To begin with, we are focussing on 
         building community web portals, starting with the Toronto LGBTQ+ population (“Queer Toronto”). 
@@ -12,6 +12,13 @@
         Project Pink Book; a list of professionals and businesses who are LGBTQ+ allies. There are other 
         websites that list some LGBTQ+ organizations and events in Toronto, but no websites provide a 
         comprehensive list.</p>
+      <p>It is free to list any type of organization or event on the Queer Toronto website. Simply go to the registration
+        section and create a new organization, then you will be able to add new events. The revenue for this
+        site will initially come from banner ads. Eventually the primary revenue will switch to a subscription
+        based model, where users will pay $7 / month. Paying the subscription fee will turn off the banner ads, 
+        enable voting as
+        a member of a co-operative, and provide some greater access to the site.
+      </p>
       <p>Ashroh Enterprises is a triple-bottom-line company that will be a hybrid customer/employee 
         co-operative. Normally companies have written into their corporate charter that they have a 
         fiduciary duty to maximize shareholder profits above all else - the bottom line. This is why 
@@ -19,17 +26,20 @@
         profits - they have a legal duty to do so - written in a legally binding contract between the 
         company and the shareholders. This company, Ashroh Enterprises, is different. It has three bottom 
         lines that will be balanced. One is to maximize shareholder profit; one is to help the communities 
-        it operates in; and one is to provide stewardship of the environment in its operations. Furthermore, 
+        it operates in; and one is to provide stewardship of the environment in its operations.</p>
+      <p>Furthermore, 
         this company is a partial co-op. Three members of the board of directors will be elected by 
         employees to represent employees needs. People will pay $7 / month to become a consumer-member of 
-        the company. When they do this they will get greater access to the site and the advertisements will 
-        be turned off. Also, they will be able to vote on, and run for, one of the three members of the 
+        the company. When they do this, in addition to having banner ads turned off and getting greater
+        access to the site, people will be able to vote on, and run for, one of the three members of the 
         board of directors elected from the consumer-members. Furthermore, the board of directors will 
         choose some decisions to be made by a vote of employees, some to be made by the board of directors 
         itself, and some to be made by a vote of customer-members. The privacy policy is an example of a 
         matter that would be decided by a vote of customer-members.</p>
       <p>Form more information see the main Ashroh Enterprises Corporate website: 
-        <a href="http://www.ashroh.com">www.ashroh.com</a>
+        <a href="http://www.ashroh.com">www.ashroh.com</a>. Note that this website and company are currently
+        in the very early stages. If you have any suggestions feel free to make them in the contact form on
+        this website.
       </p>
     </div>
   </div>
